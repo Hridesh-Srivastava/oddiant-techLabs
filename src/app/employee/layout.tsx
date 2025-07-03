7718c284bf1c755535c6c680fb3ea0b445a4df13
Hridesh-Srivastava <hrideshsrivastavaa@outlook.com>
2025-07-04 00:10:59 +0530

    refactor: simplify EmployeeLayout by using destructured props and remove withAuth wrapper

--- src/app/employee/layout.tsx
+++ src/app/employee/layout.tsx
@@ -1,19 +1,15 @@
 import type { ReactNode } from "react";
 import { Toaster } from "sonner";
-import withAuth from "@/components/auth/withAuth";
 
-// Define props type for layout
 interface LayoutProps {
   children: ReactNode;
 }
 
-const EmployeeLayout = (props: LayoutProps) => {
+export default function EmployeeLayout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-gray-50">
       <Toaster position="top-center" />
-      {props.children}
+      {children}
     </div>
   );
-};
-
-export default withAuth(EmployeeLayout, "employee");
+}
